c0e9832372d74fd28380916c8c13845f1f984154
Fernando Pereira Lisboa <fernandopereiralisboa@gmail.com>
2018-01-23 18:52:34 -0200

    Added Order Cart

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -4,17 +4,37 @@ import { MEAT_API } from '../app.api';
 import { ErrorHandler } from '../app.error-handler';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item.model';
+
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
 @Injectable()
 
 export class RestaurantsService {
-  constructor(private http: Http){}
+  constructor(private http: Http) {}
 
   restaurants(): Observable<Restaurant[]> {
     return this.http.get(`${MEAT_API}/restaurants`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError);
+  }
+
+  retaurantById(id: string): Observable<Restaurant> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError);
+  }
+
+  reviewsOfRestaurant(id: string): Observable<any> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+    .map(response => response.json())
+    .catch(ErrorHandler.handleError);
+  }
+
+  menuOfRestaurant(id: string): Observable<MenuItem[]> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+    .map(response => response.json())
+    .catch(ErrorHandler.handleError);
   }
 }
