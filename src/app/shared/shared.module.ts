a4cce54aba465c56445dfaffe5f5ab86f50d8f94
Fernando Pereira Lisboa <fernandopereiralisboa@gmail.com>
2018-01-24 14:57:33 -0200

    Added SnackBar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,18 +9,20 @@ import { RatingComponent } from './rating/rating.component';
 import { OrderService } from '../order/order.service';
 import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart.service';
 import { RestaurantsService } from '../restaurants/restaurants.service';
+import { NotificationService } from './messages/notification.service';
+import { SnackbarComponent } from './messages/snackbar/snackbar.component';
 
 @NgModule({
-  declarations: [InputComponent, RadioComponent, RatingComponent],
+  declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
-  exports: [InputComponent, RadioComponent, RatingComponent, CommonModule, FormsModule, ReactiveFormsModule]
+  exports: [InputComponent, RadioComponent, RatingComponent, CommonModule, FormsModule, ReactiveFormsModule, SnackbarComponent]
 })
 
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: SharedModule,
-      providers: [OrderService, ShoppingCartService, RestaurantsService]
+      providers: [OrderService, ShoppingCartService, RestaurantsService, NotificationService]
     };
   }
 }
